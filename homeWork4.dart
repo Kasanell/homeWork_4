d88b754ec2bda9b24e5631ce1cc4fa7966a3bbde
kasanell <bredxilx@gmail.com>
2023-05-23 17:23:10 +0600

    first commit

---
+++ homeWork4.dart
@@ -0,0 +1,64 @@
+void main() {
+  int a = 10;
+  if (a == 10) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  int a1 = 0;
+  if (a1 == 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  int a2 = 0;
+  if (a2 > 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  int a3 = 0;
+  if (a3 < 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  int a4 = 0;
+  if (a4 >= 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  int a5 = 0;
+  if (a5 <= 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  int a6 = 0;
+  if (a6 != 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  String a7 = 'test';
+  if (a7 == 'test') {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  dynamic a8 = '1';
+  if (a8 == "1" || a8 == 1) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+}
